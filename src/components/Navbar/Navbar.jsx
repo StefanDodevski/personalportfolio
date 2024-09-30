9d473e9632aef4b4eaf3a4b4f636956497075b40
StefanDodevski <stefan_dodevski31@yahoo.com>
2024-09-30 12:51:56 +0200

    Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,13 @@
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <ul className="navbar_navlinks container">
+        <li>Featured Projects</li>
+        <li>Experience</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
